c55736278551ef413687398d020e9ee61fc0d434
snowde <d.snow@live.com>
2017-01-26 19:41:20 +1300

    Update just_python.py

--- just_python.py
+++ just_python.py
@@ -2,3 +2,9 @@ bloom = train_new.reset_index()
 bloom.reindex(train_new["id"])
 train_new = train_new.fillna(a_mean)
 id_df = train_new[col].groupby('id').agg([np.mean, np.std, len]).reset_index()
+
+for o in range(len(train_new)):
+id_df = bloom[col].groupby('id')
+a_mean = id_df.mean().reset_index()
+train_new = train_new.fillna(a_mean)
+sum(train_new.isnull().sum(axis=1))
