89bcb1043bfa07ff64b4d9e5309c578455c38f67
snowde <d.snow@live.com>
2017-01-29 22:28:40 +1300

    Update snippets.py

--- snippets.py
+++ snippets.py
@@ -1,1 +1,12 @@
+# Supress unnecessary warnings so that presentation looks clean
+import warnings
+warnings.filterwarnings('ignore')
+
+import pandas #provides data structures to quickly analyze data
+#Since this code runs on Kaggle server, data can be accessed directly in the 'input' folder'
+
+#Save the id's for submission file
+ID = dataset_test['id']
+
+# iloc creates a way for reading in a dataframe based on columns and rows.
 
