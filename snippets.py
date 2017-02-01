4544c902a1cb931f121f9c46cd83536ccaebedde
snowde <d.snow@live.com>
2017-02-01 17:27:12 +1300

    Update snippets.py

--- snippets.py
+++ snippets.py
@@ -17,4 +17,21 @@ Honestly I would scratch all this and install python, pip and virtualenv using h
 
 Editing Bash:
   
-  Honestly I would scratch all this and install python, pip and virtualenv using homebrew so everything is in /usr/local/bin/
+touch ~/.bash_profile; open ~/.bash_profile
+
+what I have:
+
+# Setting PATH for Python 3.5
+# The original version is saved in .bash_profile.pysave
+PATH="/Library/Frameworks/Python.framework/Versions/3.5/bin:${PATH}"
+export PATH
+
+# added by Anaconda2 4.1.0 installer
+export PATH="/Users/dereksnow/anaconda/bin:$PATH"
+export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin   
+
+  I don't think I should use pip install, I think I should use conda install. 
+  
+  If you are using python 3, you might want to use pip3 to install to conda otherwise it might install 2.
+  
+  sudo pip3 install requests
